174066fe2b680e38e13e4d785f1ad06fb8feab34
adryvlz <adrian_velazquez-martinez@brown.edu>
2025-01-29 16:45:06 -0500

    hw1

--- hw1.py
+++ hw1.py
@@ -1,3 +1,4 @@
+#%%
 import numpy as np
 
 # HW1, problem 1 (Forward Difference)
@@ -20,18 +21,41 @@ f_prime_approx = forward_difference(f, x, h)
 # Display the result
 print (f_prime_approx)
 
-# Problem #2 (Center Difference) 
+# Problem #2 (Center Difference)
 def g(x):
-    return m.sin(x)
+    return np.sin(x)
 
-def center_diff(f, x, h):
-    return (g(x + h) + 2*g(x) + g(x-h)) / h**2
+def center_diff(g, x, h):
+    return (g(x + h) - 2 * g(x) + g(x - h)) / h**2
 
 # Update values
-x = m.pi/4
+x = np.pi / 4
 h = 0.1
 
 # Compute with central
 g_central_approx = center_diff(g, x, h)
 
 print(g_central_approx)
+
+# Problem #3 (Error Analysis in Differentiation)
+# We know that the analytical derivate of e^x should be xe^x
+# evaluate given the following steps:
+
+# update x
+x = 1
+
+h1 = 0.1 
+h2 = 0.01
+h3 = 0.001
+
+def h(x):
+    return np.exp(x)
+
+print(forward_difference(h, x, h1))
+print(forward_difference(h, x, h2))
+print(forward_difference(h, x, h3))
+
+# as seen, the difference compared to the analytical value of 2.71828182846 becomes closer as we decrease step size.
+#%%
+# Numerical Integration #
+
